b55805b7fb43663f9d2ab31adb500bc1f63a31ff
mrbinky3000 <mrbinky3000@gmail.com>
2016-02-11 17:03:11 -0500

    Trying to get to a solid base, perhaps to reuse elswhere

---
+++ app/components/Lanes.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Lane from './Lane.jsx';
+
+const Lanes = ({lanes}) => {
+	return (
+		<div className="lanes">{lanes.map(lane =>
+			<Lane className="lane" key={lane.id} lane={lane} />
+		)}</div>
+	);
+}
+Lanes.protoTypes = {
+	lanes: React.PropTypes.array
+};
+Lanes.defaultProps = {
+	lanes: []
+}
+
+export default Lanes;
